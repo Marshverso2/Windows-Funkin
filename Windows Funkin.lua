0424cc0bc51d385b007fa252851301fd87c244bb
Marshverso (OFFICIAL) <kkaio4992@gmail.com>
2025-12-21 20:01:25 -0300

    Update Windows Funkin.lua

--- Windows Funkin.lua
+++ Windows Funkin.lua
@@ -1,4 +1,4 @@
-versionW = 25
+versionW = 26
 language = os.setlocale(nil, 'collate'):lower()
 keys = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
 toType = 'NAMEUNIT'
@@ -442,6 +442,7 @@ function onTimerCompleted(tag, loops, loopsLeft)
     restartSong(false)
   end
 end
+
 
 
 
