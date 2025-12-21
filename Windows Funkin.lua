8f615f5c63f0258fd40afbcdd008489d62ffc4ac
Marshverso (OFFICIAL) <kkaio4992@gmail.com>
2025-12-21 20:37:00 -0300

    Update Windows Funkin.lua

--- Windows Funkin.lua
+++ Windows Funkin.lua
@@ -1,4 +1,4 @@
-versionW = 26.1
+versionW = 26
 keys = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
 toType = 'NAMEUNIT'
 keyCache = ''
@@ -440,6 +440,7 @@ function onTimerCompleted(tag, loops, loopsLeft)
     restartSong(false)
   end
 end
+
 
 
 
